bac3e1fd5334972967bda735a3621d9e6f3eae50
ncapel <capel.nicholas@gmail.com>
2025-05-13 11:16:33 -0400

    added user methods

--- backend/models/user.go
+++ backend/models/user.go
@@ -7,24 +7,24 @@ import (
 )
 
 type User struct {
-	ID         int64
-	Name       string
-	Password   string
-	Email      string
-	Created_at time.Time
-	Updated_at time.Time
+	ID        int64
+	Name      string
+	Password  string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 func CreateUser(db *sql.DB, name string, password string, email string) (int64, error) {
 	user := &User{
-		Name:       name,
-		Password:   password,
-		Email:      email,
-		Created_at: time.Now(),
-		Updated_at: time.Now(),
+		Name:      name,
+		Password:  password,
+		Email:     email,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
 	}
 
-	result, err := db.Exec("INSERT INTO users (name, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Name, user.Password, user.Email, user.Created_at, user.Updated_at)
+	result, err := db.Exec("INSERT INTO users (name, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Name, user.Password, user.Email, user.CreatedAt, user.UpdatedAt)
 	if err != nil {
 		return 0, fmt.Errorf("CreateUser: %v", err)
 	}
@@ -40,11 +40,25 @@ func CreateUser(db *sql.DB, name string, password string, email string) (int64, 
 func FindUserByID(db *sql.DB, id int64) (User, error) {
 	var user User
 	row := db.QueryRow("SELECT * FROM users WHERE id = ?", id)
-	if err := row.Scan(&user.ID, &user.Name, &user.Password, &user.Email, &user.Created_at, &user.Updated_at); err != nil {
+	if err := row.Scan(&user.ID, &user.Name, &user.Password, &user.Email, &user.CreatedAt, &user.UpdatedAt); err != nil {
 		if err == sql.ErrNoRows {
 			return user, fmt.Errorf("FindUserByID %d: User not found", id)
 		}
 		return user, fmt.Errorf("FindUserByID %d: %v", id, err)
 	}
 	return user, nil
+}
+
+func DeleteUser(db *sql.DB, u User) (int64, string, error) {
+	user, err := FindUserByID(db, u.ID)
+	if err != nil {
+		return 0, "", err
+	}
+
+	_, err = db.Exec("DELETE FROM users WHERE id = ?", user.ID)
+	if err != nil {
+		return 0, user.Name, fmt.Errorf("DeleteUser: %v", err)
+	}
+
+	return user.ID, "Deleted: " + user.Name, nil
 }
