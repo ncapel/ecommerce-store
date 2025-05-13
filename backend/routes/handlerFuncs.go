0c266cdf01e1460d34335055febfa19b194566ed
ncapel <capel.nicholas@gmail.com>
2025-05-13 18:30:58 -0400

    feat: added route and handler to delete user

--- backend/routes/handlerFuncs.go
+++ backend/routes/handlerFuncs.go
@@ -31,3 +31,26 @@ func handleNewUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "User created with ID: %d", id)
 }
+
+func handleDelUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	if user.ID < 0 || user.ID > 2147483647 {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	id, _, err := models.DeleteUser(config.Db, user)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error deleting user: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "User deleted with ID: %d", id)
+}
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -7,5 +7,6 @@ import (
 func InitRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /users", handleNewUser)
+	mux.HandleFunc("DELETE /users", handleDelUser)
 	return mux
 }
