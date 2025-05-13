89c2266575b439f94db580de850aa6013183da16
ncapel <capel.nicholas@gmail.com>
2025-05-13 18:07:27 -0400

    bugfix: corrected typo in table name

--- backend/models/user.go
+++ backend/models/user.go
@@ -24,7 +24,7 @@ func CreateUser(db *sql.DB, name string, password string, email string) (int64, 
 		UpdatedAt: time.Now(),
 	}
 
-	result, err := db.Exec("INSERT INTO users (name, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Name, user.Password, user.Email, user.CreatedAt, user.UpdatedAt)
+	result, err := db.Exec("INSERT INTO user (name, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Name, user.Password, user.Email, user.CreatedAt, user.UpdatedAt)
 	if err != nil {
 		return 0, fmt.Errorf("CreateUser: %v", err)
 	}
@@ -39,7 +39,7 @@ func CreateUser(db *sql.DB, name string, password string, email string) (int64, 
 
 func FindUserByID(db *sql.DB, id int64) (User, error) {
 	var user User
-	row := db.QueryRow("SELECT * FROM users WHERE id = ?", id)
+	row := db.QueryRow("SELECT * FROM user WHERE id = ?", id)
 	if err := row.Scan(&user.ID, &user.Name, &user.Password, &user.Email, &user.CreatedAt, &user.UpdatedAt); err != nil {
 		if err == sql.ErrNoRows {
 			return user, fmt.Errorf("FindUserByID %d: User not found", id)
@@ -55,7 +55,7 @@ func DeleteUser(db *sql.DB, u User) (int64, string, error) {
 		return 0, "", err
 	}
 
-	_, err = db.Exec("DELETE FROM users WHERE id = ?", user.ID)
+	_, err = db.Exec("DELETE FROM user WHERE id = ?", user.ID)
 	if err != nil {
 		return 0, user.Name, fmt.Errorf("DeleteUser: %v", err)
 	}
