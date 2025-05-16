6984691ecb83a9a191c52a750cd75080a7de4d3b
ncapel <capel.nicholas@gmail.com>
2025-05-15 21:25:35 -0400

    feat: added password hashing, password helper funcs

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,7 +3,10 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/ncapel/ecommerce-store/utils"
 )
 
 type User struct {
@@ -15,6 +18,12 @@ type User struct {
 	UpdatedAt time.Time
 }
 
+type UserPatch struct {
+	Name     *string `json:"name,omitempty"`
+	Email    *string `json:"email,omitempty"`
+	Password *string `json:"password,omitempty"`
+}
+
 func CreateUser(db *sql.DB, name string, password string, email string) (int64, error) {
 	user := &User{
 		Name:      name,
@@ -24,7 +33,8 @@ func CreateUser(db *sql.DB, name string, password string, email string) (int64, 
 		UpdatedAt: time.Now(),
 	}
 
-	result, err := db.Exec("INSERT INTO user (name, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Name, user.Password, user.Email, user.CreatedAt, user.UpdatedAt)
+	hashedPassword := utils.HashAndSalt([]byte(user.Password))
+	result, err := db.Exec("INSERT INTO user (name, password, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?)", user.Name, hashedPassword, user.Email, user.CreatedAt, user.UpdatedAt)
 	if err != nil {
 		return 0, fmt.Errorf("CreateUser: %v", err)
 	}
@@ -61,4 +71,53 @@ func DeleteUser(db *sql.DB, u User) (int64, string, error) {
 	}
 
 	return user.ID, "Deleted: " + user.Name, nil
+}
+
+func UpdateUserPatch(db *sql.DB, id int64, p UserPatch) error {
+	if db == nil {
+		return fmt.Errorf("db is nil")
+	}
+
+	sets := []string{}
+	args := []interface{}{}
+
+	if p.Name != nil {
+		sets = append(sets, "name = ?")
+		args = append(args, *p.Name)
+	}
+	if p.Email != nil {
+		sets = append(sets, "email = ?")
+		args = append(args, *p.Email)
+	}
+	if p.Password != nil {
+		hashedPassword := utils.HashAndSalt([]byte(*p.Password))
+		sets = append(sets, "password = ?")
+		args = append(args, hashedPassword)
+	}
+
+	sets = append(sets, "updated_at = ?")
+	args = append(args, time.Now())
+
+	if len(sets) == 1 {
+		return nil
+	}
+
+	query := fmt.Sprintf(
+		"UPDATE user SET %s WHERE id = ?",
+		strings.Join(sets, ", "),
+	)
+	args = append(args, id)
+
+	res, err := db.Exec(query, args...)
+	if err != nil {
+		return fmt.Errorf("UpdateUserPatch exec: %v", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("UpdateUserPatch rows: %v", err)
+	}
+	if rows == 0 {
+		return fmt.Errorf("no user found with id %d", id)
+	}
+	return nil
 }
